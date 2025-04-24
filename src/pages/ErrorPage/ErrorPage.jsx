dd8c0a40c428ad534ff5b08e3730583f6d549c48
Maleja2023 <murciaalejandra528@gmail.com>
2025-04-23 22:10:25 -0500

    agregué contenido en errorPage

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
-import './ErrorPage.css'
-const ErrorPage = () => {
-  return (
-    <div className='title'>ErrorPage</div>
-  )
-}
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import './ErrorPage.css';
 
-export default ErrorPage
+const ErrorPage = () => {
+  const navigate = useNavigate();
+
+  const goToHome = () => {
+    navigate('/');
+  };
+
+  return (
+    <div className="error-page">
+      <h1>404 - Página no encontrada</h1>
+      <p>Lo sentimos, la página que buscas no existe.</p>
+      <button className="error-button" onClick={goToHome}>
+        Devolver a la página principal
+      </button>
+    </div>
+  )
+};
+
+export default ErrorPage;
